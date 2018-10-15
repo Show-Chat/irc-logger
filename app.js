59e5aa4b2ba79c1810e43999047fa15ba9d9d1bc
appletrapz <44152967+appletrapz@users.noreply.github.com>
2018-10-15 11:34:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,10 @@
 var express = require('express')
   , cons = require('consolidate')
   , routes = require('./routes')
-  , http = require('http');
+  , http = require("http");
+    setInterval(function() {
+    http.get("http://jaredp.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 
 var app = express();
 
